430fbbb145524618fdea82e94e3256eb2f86f5da
Feruzbek Baqoyev <baqoyevferuzbek1005@gmail.com>
2024-03-05 22:47:09 +0500

    new file

---
+++ #14_Dictionary/#1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 20:57:51 2024
+
+@author: baqoyev
+Maqsad: Lug`at bilan tanishuv 
+"""
+
+# Lug`at bilan ishlash
+
+car = {'mashina' : 'ferrari', 'rang':'qizil'}
+#print(car['mashina'])
+#print(car['rang'])
+
+
+talaba = {'ism':'Feruzbek','stipendiya':'bor','yil':'2005','yosh':'19'}
+#print(f"{talaba['ism'].title()}, {talaba['yosh'].strip()} yoshda")
+#print(talaba)
+
+
+# Yangi juftlik qo`shish
+talaba['kurs'] = 1
+talaba['fakultet'] = 'Kompyuter injiniringi'
+#print(talaba)
+
+# BO`SH LUG`AT
+
+talaba1 = {}
+talaba1['ism'] = 'Feruzbek'
+talaba1['familiya'] = 'Baqoyev'
+talaba1['kurs'] = 1 
+talaba1['fakultet'] = 'Kompyuter injiniringi'
+#print(talaba1)
+
+# QIYMATNI O`ZGARTIRISH
+talaba1['kurs'] = 2
+#print(talaba1)
+
+# KALIT SO`Z-QIYMAT JUFTLIGINI O`CHIRISH
+
